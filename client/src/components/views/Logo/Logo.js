75d8526d3bc6bdee46a0596753562dd7425d84b9
jiho9505 <sun_days@naver.com>
2020-11-08 12:28:48 +0900

    updated logo

--- client/src/components/views/Logo/Logo.js
+++ client/src/components/views/Logo/Logo.js
@@ -8,7 +8,8 @@ function Logo() {
     return (
         <div style={{  textAlign:'center' }}>
             <Title level={3} >
-            <SmileTwoTone twoToneColor="#52c41a"/> Find Good Doctors <SmileTwoTone twoToneColor="#52c41a"/></Title>
+            <SmileTwoTone twoToneColor="#52c41a"/> 숨은명의찾기 <SmileTwoTone twoToneColor="#52c41a"/></Title>
+            <div><h5>-Find Good Doctors-</h5></div>
         </div>
     )
 }
