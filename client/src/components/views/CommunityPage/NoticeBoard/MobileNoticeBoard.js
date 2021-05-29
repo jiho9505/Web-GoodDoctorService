f2221834b313f73fca4eb4f275e97fec57475f83
jiho9505 <sun_days@naver.com>
2021-05-29 14:16:59 +0900

    Updated Web board Loading

--- client/src/components/views/CommunityPage/NoticeBoard/MobileNoticeBoard.js
+++ client/src/components/views/CommunityPage/NoticeBoard/MobileNoticeBoard.js
@@ -1,19 +1,15 @@
 import React from 'react'
-import { List, Badge, Empty } from 'antd';
+import { List, Badge, Empty, Spin } from 'antd';
 import Axios from 'axios'
 import moment from 'moment'
 import { Link } from 'react-router-dom';
+import { LoadingOutlined } from '@ant-design/icons';
 
 
 
 function MobileNoticeBoard(props) {
-
     const list = props.list
-    
-{/* <div style={{display:'flex', justifyContent:'center', alignItems:'center', marginTop: '30px'}}>
-                  <Spin indicator={antIcon} />
-                </div> */}
-    
+    const antIcon = <LoadingOutlined style={{ fontSize: 24 }} spin />
     
     const clickHandler = (id) => {
         let body = {
@@ -26,41 +22,57 @@ function MobileNoticeBoard(props) {
                }
              })
       }
+
+    const renderBoard = () => {
+
+      if(props.isLoading){
+
+        return <div style={{display:'flex', justifyContent:'center', alignItems:'center', marginTop: '30px'}}>
+                <Spin indicator={antIcon} />
+                </div>
+      }else{
+        if(list){
+          if(list.length>0){
+            return <List
+                      dataSource={
+                          list
+                      }
+                      bordered
+                      renderItem={item => (
+                        <List.Item
+                          style={{paddingLeft:'5px', paddingRight:'5px'}}
+                          key={item.id}
+                          actions={[
+                              <Link onClick={()=>clickHandler(item._id)} to={`community/${item._id}`}>
+                                  <span style={{color:'#00000073'}}>댓글</span>
+                                  <Badge className="site-badge-count-109" count={item.commentCount} style={{ backgroundColor: '#52c41a' }}>
+                                  
+                              </Badge>
+                              </Link>
+                              
+                  
+                          ]}
+                        >
+                          <List.Item.Meta
+                            title={<Link style={{fontWeight:'bolder' , fontFamily : 'Noto Sans'}} onClick={()=>clickHandler(item._id)} to={`community/${item._id}`}>
+                                      <span>{item.title}</span>
+                                    
+                                  </Link>}
+                            description={item.writer.nickname+' | '+moment(item.createdAt).format("YYYY-MM-D")+' | 조회수:'+item.view+' | 추천:'+item.like}
+                          />
+                        </List.Item>
+                      )}
+                    />
+          }else{
+            return <Empty/>   
+          }
+        }      
+      }
+    }
       
   return (
     <div>
-      { list && list.length>0 ?
-      <List
-        dataSource={
-            list
-        }
-        bordered
-        renderItem={item => (
-          <List.Item
-            style={{paddingLeft:'5px', paddingRight:'5px'}}
-            key={item.id}
-            actions={[
-                <Link onClick={()=>clickHandler(item._id)} to={`community/${item._id}`}>
-                    <span style={{color:'#00000073'}}>댓글</span>
-                    <Badge className="site-badge-count-109" count={item.commentCount} style={{ backgroundColor: '#52c41a' }}>
-                    
-                </Badge>
-                </Link>
-                
-    
-            ]}
-          >
-            <List.Item.Meta
-              title={<Link style={{fontWeight:'bolder' , fontFamily : 'Noto Sans'}} onClick={()=>clickHandler(item._id)} to={`community/${item._id}`}>
-                        <span>{item.title}</span>
-                       
-                     </Link>}
-              description={item.writer.nickname+' | '+moment(item.createdAt).format("YYYY-MM-D")+' | 조회수:'+item.view+' | 추천:'+item.like}
-            />
-          </List.Item>
-        )}
-      />
-        : <Empty/>  }
+      {renderBoard()}
     </div>
   )}
 
