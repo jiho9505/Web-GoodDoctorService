fe66d9dd7c4a66cbef15c8743d55508827ae5e1c
jiho9505 <sun_days@naver.com>
2021-05-30 12:05:25 +0900

    Replace var with ES6

--- server/models/User.js
+++ server/models/User.js
@@ -38,7 +38,7 @@ const userSchema = mongoose.Schema({
 
 
 userSchema.pre('save', function( next ) {
-    var user = this;
+    const user = this;
     
     if(user.isModified('password')){    
         
@@ -64,9 +64,9 @@ userSchema.methods.comparePassword = function(plainPassword,cb){
 }
 
 userSchema.methods.generateToken = function(cb) {
-    var user = this;
-    var token =  jwt.sign(user._id.toHexString(),'secret')
-    var oneHour = moment().add(1, 'hour').valueOf();
+    const user = this;
+    const token =  jwt.sign(user._id.toHexString(),'secret')
+    const oneHour = moment().add(1, 'hour').valueOf();
 
     user.tokenExp = oneHour;
     user.token = token;
@@ -77,7 +77,7 @@ userSchema.methods.generateToken = function(cb) {
 }
 
 userSchema.statics.findByToken = function (token, cb) {
-    var user = this;
+    const user = this;
 
     jwt.verify(token,'secret',function(err, decode){
         user.findOne({"_id":decode, "token":token}, function(err, users){
@@ -88,10 +88,10 @@ userSchema.statics.findByToken = function (token, cb) {
 }
 
 userSchema.methods.updateTokenExp = function(cb) {
-    var user = this;
-    var oneHour = moment().add(24, 'hour').valueOf();
+    const user = this;
+    const oneDay = moment().add(24, 'hour').valueOf();
     
-    user.tokenExp = oneHour;
+    user.tokenExp = oneDay;
     user.save(function (err, user){
         if(err) return cb(err)
         cb(null, user);
